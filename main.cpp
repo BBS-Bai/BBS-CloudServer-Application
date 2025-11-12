271cc01b3871a93ab0c9efa9d23fa7be493e3017
BBS-Bai <323764636@qq.com>
2025-11-12 22:42:33 +0800

    BBS-CloudServer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "src/mainwindow/mainwindow.h"
+/*#include "src/mainwindow/mainwindow.h"
 #include <QApplication>
 #include <QStyleFactory>
 #include "src/core/logger.h"
@@ -37,3 +37,5 @@ int main(int argc, char *argv[]) {
 
     return app.exec();
 }
+*/
+
